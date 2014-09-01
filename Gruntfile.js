909f07bd023b18d72f3b5d7cd184c063bff9fa83
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2014-09-01 21:35:43 +0100

    Header comment typo correction

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /*
- * grunt-set-app-mode
- * https://github.com/raffishquartan/grunt-set-app-mode
+ * grunt-version-file
+ * https://github.com/raffishquartan/grunt-version-file
  *
  * Copyright (c) 2014 raffishquartan
  * Licensed under the MIT license.
