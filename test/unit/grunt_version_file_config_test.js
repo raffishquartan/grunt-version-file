f66fdd3920b71a8b60999a2c46a02ec5426c4b96
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2014-09-02 08:17:06 +0100

    Unit test stubs for lib classes

---
+++ test/unit/grunt_version_file_config_test.js
@@ -0,0 +1,13 @@
+/*
+ * grunt-version-file
+ * https://github.com/raffishquartan/grunt-version-file
+ *
+ * Copyright (c) 2014 raffishquartan
+ * Licensed under the MIT license.
+ */
+
+ "use strict";
+
+describe("GruntVersionFileConfig", function() {
+
+});
