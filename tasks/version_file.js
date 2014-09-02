74e09471314e49bbc58d8bf057538c2c93b2a663
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2014-09-02 08:15:02 +0100

    tasks/version_file.js - Grunt plugin registration

---
+++ tasks/version_file.js
@@ -0,0 +1,12 @@
+/*
+ * grunt-version-file
+ * https://github.com/raffishquartan/grunt-version-file
+ *
+ * Copyright (c) 2014 raffishquartan
+ * Licensed under the MIT license.
+ */
+
+"use strict";
+
+var grunt_version_file = require("../lib/grunt_version_file.js");
+module.exports = grunt_version_file.register_with_grunt;
