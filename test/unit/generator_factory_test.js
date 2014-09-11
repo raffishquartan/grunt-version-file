9c3ec2ec57e3f84e7f0334b26c5fdd04f49c7f41
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2014-09-11 20:39:17 +0100

    GeneratorFactory unit tests

--- test/unit/generator_factory_test.js
+++ test/unit/generator_factory_test.js
@@ -7,31 +7,79 @@
  */
 // Allow indirectly-executed (should-executed) function literals to pass jshint IIFE warning:
 /*jshint -W068 */
+// Allow expressions (...should.be.a.Foo;)
+/*jshint -W030 */
 
  "use strict";
 
 describe("GeneratorFactory", function() {
   var should = require("should");
-  var GeneratorBase = require("../../lib/generator_factory.js");
+  var GeneratorFactory = require("../../lib/generator_factory.js");
 
-  it.skip("has a create method", function() {
+  it("has a create method", function() {
+    var gf = new GeneratorFactory();
+    gf.create.should.be.a.Function;
   });
 
-  it.skip("... that requires options.label", function() {
+  it("... that requires options.label", function() {
+    var gf = new GeneratorFactory();
+    var config_without_label = { value: function() {} };
+    (function() {
+      gf.create(config_without_label);
+    }).should.throw();
   });
 
-  it.skip("... that requires options.value", function() {
+  it("... that requires options.label to be a function", function() {
+    var gf = new GeneratorFactory();
+    var config_without_label_function = { value: function() {}, label: "label" };
+    (function() {
+      gf.create(config_without_label_function);
+    }).should.throw();
   });
 
-  it.skip("... that allows options.init to specify an init function", function() {
+  it("... that requires options.value", function() {
+    var gf = new GeneratorFactory();
+    var config_without_value = { label: function() {} };
+    (function() {
+      gf.create(config_without_value);
+    }).should.throw();
   });
 
-  it.skip("creates generators with a default init function", function() {
+  it("... that requires options.label to be a function", function() {
+    var gf = new GeneratorFactory();
+    var config_without_value_function = { value: "value", label: function() {} };
+    (function() {
+      gf.create(config_without_value_function);
+    }).should.throw();
   });
 
-  it.skip("creates generators with a value function that returns a promise", function() {
+  it("... creates objects with a value and label function", function() {
+    var gf = new GeneratorFactory();
+    var config = { label: function() {}, value: function() {} };
+    var generator = gf.create(config);
+    generator.label.should.be.a.Function;
+    generator.value.should.be.a.Function;
   });
 
-  it.skip("creates generators with a label function that returns string", function() {
+  it("... that allows options.init to specify an init function", function() {
+    var gf = new GeneratorFactory();
+    var config_with_init = { label: function() {}, value: function() {}, init: function() {} };
+    var generator = gf.create(config_with_init);
+    generator.init.should.be.a.Function;
+  });
+
+  it("... that creates generators with a default init function", function() {
+    var gf = new GeneratorFactory();
+    var config = { label: function() {}, value: function() {} };
+    var generator = gf.create(config);
+    generator.init.should.be.a.Function;
+  });
+
+  it("... that throws an error if extra options are present", function() {
+    var gf = new GeneratorFactory();
+    var config_with_extra = { value: function() {}, label: function() {}, foo: function() {} };
+    (function() {
+      gf.create(config_with_extra);
+    }).should.throw();
   });
 });
