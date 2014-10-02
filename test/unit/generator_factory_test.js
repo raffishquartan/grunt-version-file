b9aa921a6e4c7cf2934039b701949507d4b8b7c8
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2014-10-02 23:37:08 +0100

    GeneratorFactory updated bc gen's now have label_value function

--- test/unit/generator_factory_test.js
+++ test/unit/generator_factory_test.js
@@ -21,56 +21,40 @@ describe("GeneratorFactory", function() {
     gf.create.should.be.a.Function;
   });
 
-  it("... that requires options.label", function() {
+  it("... that requires options.label_value", function() {
     var gf = new GeneratorFactory();
-    var config_without_label = { value: function() {} };
+    var config_without_label_value = {};
     (function() {
-      gf.create(config_without_label);
+      gf.create(config_without_label_value);
     }).should.throw();
   });
 
-  it("... that requires options.label to be a function", function() {
+  it("... that requires options.label_value to be a function", function() {
     var gf = new GeneratorFactory();
-    var config_without_label_function = { value: function() {}, label: "label" };
+    var config_without_label_value_function = { label_value: function() {}, label: "label" };
     (function() {
-      gf.create(config_without_label_function);
+      gf.create(config_without_label_value_function);
     }).should.throw();
   });
 
-  it("... that requires options.value", function() {
+  it("... creates objects with a label_value function", function() {
     var gf = new GeneratorFactory();
-    var config_without_value = { label: function() {} };
-    (function() {
-      gf.create(config_without_value);
-    }).should.throw();
-  });
-
-  it("... that requires options.label to be a function", function() {
-    var gf = new GeneratorFactory();
-    var config_without_value_function = { value: "value", label: function() {} };
-    (function() {
-      gf.create(config_without_value_function);
-    }).should.throw();
-  });
-
-  it("... creates objects with a value and label function", function() {
-    var gf = new GeneratorFactory();
-    var config = { label: function() {}, value: function() {} };
+    var config = { label_value: function() {} };
     var generator = gf.create(config);
-    generator.label.should.be.a.Function;
-    generator.value.should.be.a.Function;
+    generator.label_value.should.be.a.Function;
   });
 
   it("... that allows options.init", function() {
     var gf = new GeneratorFactory();
-    var config_with_init = { label: function() {}, value: function() {}, init: function() {} };
+    var config_with_init = { label_value: function() {}, init: function() {} };
     var generator = gf.create(config_with_init);
     generator.init.should.be.a.Function;
+    generator.label_value.should.be.a.Function;
   });
 
   it("... that requires options.init to be a function", function() {
     var gf = new GeneratorFactory();
-    var config_with_nonfunction_init = { label: function() {}, value: function() {}, init: "init" };
+    var config_with_nonfunction_init = { label_value: function() {}, init: "init" };
     (function() {
       gf.create(config_with_nonfunction_init);
     }).should.throw();
@@ -78,14 +62,14 @@ describe("GeneratorFactory", function() {
 
   it("... that creates generators with a default init function", function() {
     var gf = new GeneratorFactory();
-    var config = { label: function() {}, value: function() {} };
+    var config = { label_value: function() {} };
     var generator = gf.create(config);
     generator.init.should.be.a.Function;
   });
 
   it("... that throws an error if extra options are present", function() {
     var gf = new GeneratorFactory();
-    var config_with_extra = { value: function() {}, label: function() {}, foo: function() {} };
+    var config_with_extra = { label_value: function() {}, foo: function() {} };
     (function() {
       gf.create(config_with_extra);
     }).should.throw();
