10aeda3f38ee0850cf13c7403718f6fc21d13d1f
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2014-10-12 13:29:03 +0100

    git_revision --> git_describe (for clarity)

--- test/unit/generators/git_describe_test.js
+++ test/unit/generators/git_describe_test.js
@@ -12,18 +12,18 @@
 
 "use strict";
 
-describe("GitRevisionGenerator", function() {
+describe("GitDescribeGenerator", function() {
   var should = require("should");
-  var git_revision = require("../../../lib/generators/git_revision");
+  var git_describe = require("../../../lib/generators/git_describe");
 
   it("specifies a label_value method that returns a then'able object", function() {
-    git_revision.label_value.should.be.a.Function;
-    git_revision.label_value().should.be.a.Object;
-    git_revision.label_value().then.should.be.a.Function;
+    git_describe.label_value.should.be.a.Function;
+    git_describe.label_value().should.be.a.Object;
+    git_describe.label_value().then.should.be.a.Function;
   });
 
   it("the then'able object resolves to an object with a label and value", function() {
-    git_revision.label_value().then(function(result) {
+    git_describe.label_value().then(function(result) {
       result.label.should.not.be.undefined;
       result.label.should.be.a.String;
       result.value.should.not.be.undefined;
